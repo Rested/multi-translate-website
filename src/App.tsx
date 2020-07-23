38b2594e0d8ab8f787d054dbee12a81f3c24683f
Reuben Thomas-Davis <reubenestd@gmail.com>
2020-07-23 14:24:30 +0100

    :sparkles: improve title bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import './App.css';
-import {Typography, ThemeProvider, createMuiTheme, Tabs, Paper, Tab, Badge} from "@material-ui/core";
+import {Typography, ThemeProvider, createMuiTheme, Tabs, Paper, Tab, Badge, Grid} from "@material-ui/core";
 import logo from "./logo.svg"
 import {Favorite} from "@material-ui/icons";
 import logoTxt from "./logotxt.svg"
@@ -42,11 +42,11 @@ function App() {
     const [tab, setTab] = useState(Math.max(0, hashes.indexOf(window.location.hash)));
     const [version, setVersion] = useState("0.6.0");
     const hash = useHash()
-    useEffect(()=> {
+    useEffect(() => {
         setTab(Math.max(0, hashes.indexOf(hash)))
     }, [hash]);
 
-    useEffect(()=> {
+    useEffect(() => {
         getVersion().then(v => setVersion(v));
     }, [])
 
@@ -54,36 +54,40 @@ function App() {
         <ThemeProvider theme={theme}>
             <div className="App">
                 <header>
-                    <Typography color="textSecondary" variant="h2"
-                                style={{display: "inline-block"}}><Badge badgeContent={`Version ${version}`} anchorOrigin={{
-                        vertical: 'top',
-                        horizontal: 'right',
-                    }} color="primary">Multi-Translate</Badge></Typography>
-                    <Typography color="textSecondary"
-                               style={{display: "inline-block", marginLeft: "1rem", marginRight: "1rem"}}>made
-                        with <Favorite fontSize="inherit"/> by</Typography>
-                    <a href="https://rekon.uk">
-                        <div style={{display: "inline-block"}}>
-                            <img src={logo} style={{height: "2rem"}}/>
-                            <img src={logoTxt} style={{height: "2rem", marginLeft: "0.75rem"}}/>
-                        </div>
-                    </a>
+                    <Grid container justify="space-between" alignItems="baseline">
+                        <Grid item xs={12} sm={6}>
+                            <Typography color="textSecondary" variant="h2"
+                                        style={{display: "inline-block"}}>Multi-Translate</Typography>
+                            <Typography color="textSecondary" style={{display: "inline-block", marginLeft: "1rem"}}> Version {version}</Typography>
+                        </Grid>
+                        <Grid item xs={12} sm={6} style={{textAlign: "right"}}>
+                            <Typography color="textSecondary"
+                                        style={{display: "inline-block", marginLeft: "1rem", marginRight: "1rem"}}>made
+                                with <Favorite fontSize="inherit"/> by</Typography>
+                            <a href="https://rekon.uk">
+                                <div style={{display: "inline-block"}}>
+                                    <img src={logo} style={{height: "2rem"}}/>
+                                    <img src={logoTxt} style={{height: "2rem", marginLeft: "0.75rem"}}/>
+                                </div>
+                            </a>
+                        </Grid>
+                    </Grid>
                 </header>
                 <Paper style={{marginTop: 20, marginBottom: 40}}>
                     <Tabs centered onChange={(event: React.ChangeEvent<{}>, newValue: number) => {
                         setTab(newValue);
                         window.location.hash = hashes[newValue];
                     }} value={tab}>
-                        <Tab label="Translate" />
-                        <Tab label="About" />
-                        <Tab label="Support" />
+                        <Tab label="Translate"/>
+                        <Tab label="About"/>
+                        <Tab label="Support"/>
                     </Tabs>
                 </Paper>
                 <section style={tab !== 0 ? {display: "none"} : {}}>
                     <Translate/>
                 </section>
-                {tab === 1 ? <section><About/></section>: null}
-                {tab === 2 ? <section><Support/></section>: null}
+                {tab === 1 ? <section><About/></section> : null}
+                {tab === 2 ? <section><Support/></section> : null}
             </div>
         </ThemeProvider>
     );
